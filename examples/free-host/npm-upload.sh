7c1913b233ad7ae10bfe7298847a478631bb7875
zjcqoo <zjcqoo@gmail.com>
2022-06-07 20:14:33 +0800

    chore: npm-upload.sh 小调整

--- examples/free-host/npm-upload.sh
+++ examples/free-host/npm-upload.sh
@@ -10,34 +10,37 @@ if [[ "$*" == "" ]]; then
   exit
 fi
 
-for file in $*; do
-  hash=$(openssl dgst -sha256 -binary $file | openssl base64 -A)
+tmp=$(mktemp -d)
+
+for src in $*; do
+  hash=$(openssl dgst -sha256 -binary $src | openssl base64 -A)
   key=$(sed 's/[^a-zA-Z0-9]//g' <<< $hash | head -c 16)
 
-  path=$NPM_PKG@0.0.0-$key/index.js
-  url1=https://unpkg.com/$path
-  url2=https://cdn.jsdelivr.net/npm/$path
+  ver=0.0.0-$key
+  pkg=$NPM_PKG@$ver
+  dst=$pkg/index.js
 
-  echo "upload: $file ($hash)"
+  echo "upload: $src ($hash)"
 
   list="$list
-$hash $url1
-$hash $url2"
-
-  str=$(curl -sI $url1 | head -n1)
-  if [[ $str =~ "200" ]]; then
-    echo "file existed: $url1"
+$hash https://unpkg.com/$dst
+$hash https://cdn.jsdelivr.net/npm/$dst
+"
+  str=$(npm view $pkg name)
+  if [[ $str == $NPM_PKG ]]; then
+    echo "file existed: https://unpkg.com/$dst"
     continue
   fi
 
-  dst=$(mktemp -d)
-  cp $file $dst/index.js
+  rm -f $tmp/*
+  cp $src $tmp/index.js
 
-  echo '{"name":"'$NPM_PKG'","version":"0.0.0-'$key'"}' > $dst/package.json
-  npm publish $dst
+  echo '{"name":"'$NPM_PKG'","version":"'$ver'"}' > $tmp/package.json
+  npm publish $tmp --quiet
 
-  echo "uploaded: $url1"
-  rm -rf $dst
+  echo "uploaded: https://unpkg.com/$dst"
 done
+
+rm -rf $tmp
 
 freecdn db --import <<< "$list"
